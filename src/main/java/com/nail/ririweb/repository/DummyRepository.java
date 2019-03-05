711f15d840dc29706c488b77b469c05ae797abf3
정채환 <funchcode@gmail.com>
2019-03-05 11:54:02 +0900

    Dummy Code (Dummy)

--- src/main/java/com/nail/ririweb/repository/DummyRepository.java
+++ src/main/java/com/nail/ririweb/repository/DummyRepository.java
@@ -1,4 +1,8 @@
 package com.nail.ririweb.repository;
 
-public interface DummyRepository {
+import com.nail.ririweb.dao.Dummy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DummyRepository extends JpaRepository<Dummy, String> {
+
 }
