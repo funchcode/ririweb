56a214c8f0519651ae31504576cd737a9f2f29c6
정채환 <funchcode@gmail.com>
2019-04-05 19:01:09 +0900

    mk branch tutorials

--- src/main/java/com/nail/ririweb/repository/DummyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nail.ririweb.repository;
-
-import com.nail.ririweb.dao.Dummy;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DummyRepository extends JpaRepository<Dummy, String> {
-
-}
