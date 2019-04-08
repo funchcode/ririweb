a524330ffd8bd7aa2ac65b711be062e31d770196
정채환 <funchcode@gmail.com>
2019-04-08 23:08:04 +0900

    AWS RDS 연동

--- src/main/java/com/tutorials/webservice/service/PostsService.java
+++ src/main/java/com/tutorials/webservice/service/PostsService.java
@@ -1,11 +1,15 @@
 package com.tutorials.webservice.service;
 
+import com.tutorials.webservice.domain.posts.Posts;
 import com.tutorials.webservice.domain.posts.PostsRepository;
+import com.tutorials.webservice.dto.posts.PostsMainResponseDto;
 import com.tutorials.webservice.dto.posts.PostsSaveRequestDto;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service
@@ -15,5 +19,10 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto dto) {
         return postsRepository.save(dto.toEntity()).getId();
+    }
+
+    @Transactional
+    public List<PostsMainResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().map(PostsMainResponseDto::new).collect(Collectors.toList());
     }
 }
